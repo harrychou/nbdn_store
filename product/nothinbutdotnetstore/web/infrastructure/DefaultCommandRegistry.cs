6c9ca2e53dc34fb96deeab6e46cd24d9a406a0c8
Harry Chou <harrychou@gmail.com>
2009-10-27 16:43:33 -0400

    Implemented MissingCommand

--- product/nothinbutdotnetstore/web/infrastructure/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/infrastructure/DefaultCommandRegistry.cs
@@ -1,12 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 namespace nothinbutdotnetstore.web.infrastructure
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
+        readonly IEnumerable<Command> commands;
+
+        public DefaultCommandRegistry(IEnumerable<Command> commands)
+        {
+            this.commands = commands;
+        }
+
         public Command get_command_that_can_process(Request request)
         {
-            throw new NotImplementedException();
+            foreach (var command in commands)
+            {
+                if (command.can_handle(request)) return command;
+            }
+
+            return new MissingCommand();
         }
     }
 }
