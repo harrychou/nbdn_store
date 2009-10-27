9f0f9e4a6f1178bf25815456b09469e86eb054a5
vikasl <vikasl@hotmail.com>
2009-10-27 16:00:43 -0400

    test

--- product/nothinbutdotnetstore/web/infrastructure/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/infrastructure/DefaultCommandRegistry.cs
@@ -1,12 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 namespace nothinbutdotnetstore.web.infrastructure
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
+        private IEnumerable<Command> _commandList;
+
+        public DefaultCommandRegistry(IEnumerable<Command> commandList)
+        {
+            _commandList = commandList;
+        }
+
         public Command get_command_that_can_process(Request request)
         {
-            throw new NotImplementedException();
+            foreach(Command command in _commandList )
+            {
+                if( command.can_handle( request ))
+                {
+                    return command;
+                }
+            }
+            return null;
         }
     }
 }
