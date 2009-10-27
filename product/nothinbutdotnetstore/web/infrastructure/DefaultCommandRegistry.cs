b874e46cc55a3bebcb859988eb03706a4d1fe276
Scott Flaherty <scott@clanoflaherty.com>
2009-10-27 15:57:26 -0400

    implemented command selector

--- product/nothinbutdotnetstore/web/infrastructure/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/infrastructure/DefaultCommandRegistry.cs
@@ -1,12 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 namespace nothinbutdotnetstore.web.infrastructure
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
+        readonly IEnumerable<Command> _commands;
+
+        public DefaultCommandRegistry(IEnumerable<Command> commands)
+        {
+            _commands = commands;
+        }
+
         public Command get_command_that_can_process(Request request)
         {
-            throw new NotImplementedException();
+            foreach (var command in _commands) 
+            {
+                if (command.can_handle(request)) return command;
+            }
+
+            return null;
         }
     }
 }
