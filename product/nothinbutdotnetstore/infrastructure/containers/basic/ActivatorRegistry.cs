7a599e579d580630582b45a9e9f55a7516c41d2e
unknown <esk5cuw@.HTP.htp-inc.com>
2009-10-29 12:40:53 -0400

    activator registry

--- product/nothinbutdotnetstore/infrastructure/containers/basic/ActivatorRegistry.cs
+++ product/nothinbutdotnetstore/infrastructure/containers/basic/ActivatorRegistry.cs
@@ -1,8 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 namespace nothinbutdotnetstore.infrastructure.containers.basic
 {
-    public interface ActivatorRegistry {
+    public interface ActivatorRegistry
+    {
         InstanceActivator get_activator_for(Type type);
+    }
+
+    public class DefaultActivatorRegistry : ActivatorRegistry
+    {
+        Dictionary<Type, InstanceActivator> activator_list;
+
+        public DefaultActivatorRegistry(Dictionary<Type, InstanceActivator> activator_list)
+        {
+            this.activator_list = activator_list;
+        }
+
+        public InstanceActivator get_activator_for(Type type)
+        {
+            if (activator_list.ContainsKey(type))
+                return activator_list[type];
+            return null;
+        }
     }
 }
