8a4527425e001ef9c963ecc4b10637f62456eda7
Scott Flaherty <scott@clanoflaherty.com>
2009-10-30 09:03:14 -0400

    added contructot to auto wiring

--- product/nothinbutdotnetstore/infrastructure/containers/basic/AutoWiringInstanceActivator.cs
+++ product/nothinbutdotnetstore/infrastructure/containers/basic/AutoWiringInstanceActivator.cs
@@ -4,6 +4,13 @@ namespace nothinbutdotnetstore.infrastructure.containers.basic
 {
     public class AutoWiringInstanceActivator : InstanceActivator
     {
+        readonly ConstructorResolver _constructorResolver;
+
+        public AutoWiringInstanceActivator(ConstructorResolver constructor_resolver)
+        {
+            _constructorResolver = constructor_resolver;
+        }
+
         public object create()
         {
             throw new NotImplementedException();
