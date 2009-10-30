280469d12e082f1f9485e6b3f98903ecf5a1fee8
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2009-10-30 17:49:59 -0600

    Removed missing files

--- product/nothinbutdotnetstore/domain/Customer.cs
+++ product/nothinbutdotnetstore/domain/Customer.cs
@@ -3,7 +3,7 @@ namespace nothinbutdotnetstore.domain
     public class Customer
     {
         public string first_name { get; set; }
-        public string last_nane { get; set; }
+        public string last_name { get; set; }
         public string email_address { get; set; }
         public Address Address { get; set; }
     }
